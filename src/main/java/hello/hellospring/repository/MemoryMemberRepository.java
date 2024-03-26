5fddaa53fe84e5a1ee88e3423a6452d8adeec280
정재훈 <70499531+pleasemrlostman@users.noreply.github.com>
2024-03-26 22:44:00 +0900

    자동 의존 관계 설정

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,8 +1,10 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
+@Repository
 
 public class MemoryMemberRepository implements MemberRepository{
 
